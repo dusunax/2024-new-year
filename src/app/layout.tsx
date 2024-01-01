4641ad81223de5f92be7640e3e6d4065d7a4ebdd
Dusuna <94776135+dusunax@users.noreply.github.com>
2024-01-02 00:49:34 +0900

    feat: GA 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 import ContentsArea from "@/layout/ContentsArea";
@@ -35,7 +36,20 @@ export default function RootLayout({
           <ContentsArea>{children}</ContentsArea>
         </div>
       </body>
-      <div id="portal" />
+
+      {/* Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=YOUR_TRACKING_ID`}
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'YOUR_TRACKING_ID');
+        `}
+      </Script>
     </html>
   );
 }
